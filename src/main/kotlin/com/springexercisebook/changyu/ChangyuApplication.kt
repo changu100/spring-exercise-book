98de4753882526295154ef4e8c4404cbf1764780
changyuPark <blue_rose17@naver.com>
2022-12-19 15:37:00 +0900

    feat: init project

---
+++ src/main/kotlin/com/springexercisebook/changyu/ChangyuApplication.kt
@@ -0,0 +1,11 @@
+package com.springexercisebook.changyu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChangyuApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChangyuApplication>(*args)
+}
---
+++ src/test/kotlin/com/springexercisebook/changyu/ChangyuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springexercisebook.changyu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChangyuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
